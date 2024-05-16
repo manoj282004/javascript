5f541d3a00dc8ef380c088fe284afd789dc697c3
Manoj kumar <139623188+manoj282004@users.noreply.github.com>
2024-05-16 12:45:25 +0000

    dataconversion

--- 01_basic/03_dataconversion.js
+++ 01_basic/03_dataconversion.js
@@ -1,8 +1,16 @@
 let val="42adf";
 
-console.log(val);
+// console.log(val);
 
 let stringval=Number(val);
 
-console.log(typeof (stringval));
-console.log(stringval)
+// console.log(typeof (stringval));
+// console.log(stringval)
+
+console.log(1+2+"4"+3+4);
+console.log("2"+3+3);
+console.log(3+4);
+
+let val1="4";
+let val2=43;
+console.log(val1+val2);
