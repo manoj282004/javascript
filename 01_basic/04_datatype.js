6fd38e792a713e10afb89ca8fad0a6cc82a514d1
Manoj kumar <139623188+manoj282004@users.noreply.github.com>
2024-05-16 13:48:08 +0000

    memory allocation or datatype

--- 01_basic/04_datatype.js
+++ 01_basic/04_datatype.js
@@ -4,18 +4,18 @@
 // 1-> premitive 
 //  string,number,null undefined,symbol,boolean,bigint
 
-let val="manoj";
-console.log(typeof(val));
-let check=43;
-console.log(typeof(check));
-let Nll=null;
-console.log(typeof(Nll));
+// let val="manoj";
+// console.log(typeof(val));
+// let check=43;
+// console.log(typeof(check));
+// let Nll=null;
+// console.log(typeof(Nll));
 
-let firstval=Symbol(32);
-let secondval=Symbol(32);
-console.log(firstval==secondval);
-console.log(firstval);
-console.log(secondval);
+// let firstval=Symbol(32);
+// let secondval=Symbol(32);
+// console.log(firstval==secondval);
+// console.log(firstval);
+// console.log(secondval);
 
 
 
@@ -34,3 +34,24 @@ console.log(secondval);
 // let fun= function(){
 //     console.log("hello kese ho "); function
 // }
+
+//+++++++++++++++++++++++++++++++++++++++++++
+
+//type of memory allocation in JSON
+//1 -> stack (primitive)
+
+let firstval="manojkumar";
+let secondval=firstval;
+secondval="rashkumar";
+console.log(secondval);
+
+// 2->heap (reference or non-primitive);
+
+let obj={
+    mail:"manoj@gmail.com",
+    id:"32343",
+};
+
+let obj2=obj;
+obj2.mail="rahe@gmail.com";
+console.log(obj2.mail);
