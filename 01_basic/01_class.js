4af55d50b1e6adb9ba63e255b6720743da605240
Manoj kumar <139623188+manoj282004@users.noreply.github.com>
2024-05-16 05:44:57 +0000

    variable

---
+++ 01_basic/01_class.js
@@ -0,0 +1,10 @@
+let firstVal=323;
+const valOf=43;
+var check="manoj";
+ data="mohan";
+ 
+ check="raju";
+ let val;
+
+ 
+ console.table([firstVal,valOf,check,data,val,check]);
--- test.js
+++
@@ -1,4 +0,0 @@
-// let val=["manoj",'raja','dev','neeraj','shivkant']
-// console.log(typeof val);
-
-console.log(typeof null);
