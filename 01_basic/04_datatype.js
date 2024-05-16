471f88a0f3891864228859d013437dc41f3d063b
Manoj kumar <139623188+manoj282004@users.noreply.github.com>
2024-05-16 13:32:10 +0000

    datatype primitive or non-primitive

---
+++ 01_basic/04_datatype.js
@@ -0,0 +1,36 @@
+
+// datatype in javascrip
+
+// 1-> premitive 
+//  string,number,null undefined,symbol,boolean,bigint
+
+let val="manoj";
+console.log(typeof(val));
+let check=43;
+console.log(typeof(check));
+let Nll=null;
+console.log(typeof(Nll));
+
+let firstval=Symbol(32);
+let secondval=Symbol(32);
+console.log(firstval==secondval);
+console.log(firstval);
+console.log(secondval);
+
+
+
+
+
+
+// 2-> reference (non-primitive)
+// array,function,object;
+
+// let arr=["manoj","kumar","dheeraj","ajay","niranjan singh"];  array
+
+//  {
+              //      object
+// };
+
+// let fun= function(){
+//     console.log("hello kese ho "); function
+// }
