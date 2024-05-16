3757749460ab65ccf63bfa945e36a6d7edb074b1
Manoj kumar <139623188+manoj282004@users.noreply.github.com>
2024-05-16 06:28:32 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,12 @@
+let val="manoj";
+
+// string 
+// null
+// undefined
+// number
+// boolean
+// symbol  used for unique
+// object
+
+//console.log(typeof null); always type is object
+//console.log(typeof undefined) always type is undefined
