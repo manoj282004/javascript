e4be84ebfd26ad749e90adfe61757a08dce784ea
Manoj kumar <139623188+manoj282004@users.noreply.github.com>
2024-05-17 04:43:04 +0000

    math function

---
+++ 01_basic/math.js
@@ -0,0 +1,8 @@
+let val=43;
+
+// randdom 
+let first=40;
+let last=50;
+console.log(Math.floor(Math.random()*(last-first)+first));
+// Math.ceil(),Math.floor(),Math.round(),math.Tofixed() this is used for how many point needs after digit
+// 
